7aba7245160d16cd90e3f625f77bd786564a2130
NguyenThiLinh <nguyenlynh268@gmail.com>
2019-07-04 15:17:45 +0700

    commit layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel sdsad
+                    Laravel sdsad abc
                 </div>
 
                 <div class="links">
