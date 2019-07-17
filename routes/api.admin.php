9e9fa2f5bf72136e66140c74a2363d4669f7a2bd
NguyenThiLinh <nguyenlynh268@gmail.com>
2019-07-17 14:25:48 +0700

    MI2T-51-API-Product-List

--- routes/api.admin.php
+++ routes/api.admin.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('login', 'Admin\LoginController@login');
 
 Route::group([ 'middleware'=>'auth:admins'], function(){
-    Route::apiResource('products','ProductsController');   
+    Route::post('products','ProductsController@store');   
 });
 
-
+Route::get('products','ProductsController@index'); 
